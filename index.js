ddacd1b84143c432ec227baa08f1342b085875ed
OMR K9 <121472588+oreynosa12@users.noreply.github.com>
2023-03-24 23:16:12 -0400

    added packages needed for the app

--- index.js
+++ index.js
@@ -1,13 +1,65 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require(`fs`);
+// const generateMarkdown = require(`generateMarkdown.js`)
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt ( [
+    {
+        type: 'input',
+        message: 'What is your project name?',
+        name: 'username',
+      },
+      {
+        type: 'input',
+        message: 'What is your project description?',
+        name: 'password',
+      },
+      {
+        type: 'input',
+        message: 'How do you install it?:',
+        name: 'confirm',
+      },
+      {
+        type: 'input',
+        message: 'What is the usage?',
+        name: 'confirm',
+      },
+      {
+        type: 'input',
+        message: 'Do you want to add credits?',
+        name: 'confirm',
+      },
+      {
+        type: 'input',
+        message: 'What type of license did you use?',
+        name: 'confirm',
+      },
+]);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  if (error){
+    console.log(error)
+  }
+}  
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
+
+
+// ***PRACTICE*****
+
+// const a = process.argv[2]
+// const b = process.argv[3]
+
+// if (a === b){
+// console.log ('They are equal')
+// }
+// else{
+//   console.log('They are not equal')
+// }
+
